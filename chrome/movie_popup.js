1da98da31518a82f08a35f140878c5e40d14cffc
Bogna Knychala <bogna.ka@gmail.com>
2015-11-01 23:57:33 +0100

    Working extension

--- chrome/movie_popup.js
+++ chrome/movie_popup.js
@@ -4,7 +4,7 @@ function getCurrentTabUrl(callback) {
 		currentWindow: true
 	};
 
-	chrome.tabs.query(queryInfo, function(tabs) {
+	chrome.tabs.query(queryInfo, function (tabs) {
 		var tab = tabs[0],
 			url = tab.url;
 
@@ -19,29 +19,40 @@ function renderStatus(statusText) {
 function getMagnetLink(title) {
 	var url = "http://moviewishlist-bogna.rhcloud.com/api/status/" + title + "/1";
 	return fetch(url, {method: 'get'})
-		.then(function(response) {
+		.then(function (response) {
 			return response.json();
 		})
-		.catch(function(err) {
+		.catch(function (err) {
 			console.log(err);
 		})
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	getCurrentTabUrl(function(url) {
+document.addEventListener('DOMContentLoaded', function () {
+	getCurrentTabUrl(function (url) {
 		var title;
 
 		if (url.indexOf('www.filmweb') > -1) {
 			chrome.tabs.executeScript({
 				code: 'document.querySelector("#body h2.cap").innerText'
-			}, function(res) {
+			}, function (res) {
 				renderStatus('Getting best magnet link for title: ' + res[0]);
 				getMagnetLink(res)
-					.then(function(data) {
-						console.log(data[0]);
+					.then(function (data) {
+						document.getElementById('status').style.display = "none";
+						document.getElementById('result').style.display = "block";
+						document.getElementById('title').textContent = data[0].title;
+						document.getElementById('tag').textContent = data[0].quality.tag;
+						document.getElementById('magnet').addEventListener('click', function() {
+							var url = document.getElementById('magnet').dataset.href;
+							chrome.tabs.create({url: url});
+						}, true);
+
+						document.getElementById('magnet').dataset.href = data[0].magnetLink;
+						document.getElementById('seedersLeechers').textContent =
+							data[0].seedersAndLeechers.seeders + "/" + data[0].seedersAndLeechers.leechers;
+
 					})
-					.catch(function(err) {
-						console.log(err);
+					.catch(function (err) {
 						renderStatus("Error when fetching data");
 					});
 			});
